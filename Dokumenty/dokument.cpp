ddc6d7c2d63b9825669c4299db17d79813b17abb
Peter Polak <polpe03@ca.com>
2017-09-12 13:30:27 +0200

    mazanie spojenia fix

--- Dokumenty/dokument.cpp
+++ Dokumenty/dokument.cpp
@@ -8,16 +8,16 @@ using namespace Dokumenty;
 void Dokument::velkostDokumentuZmenena(qreal) { emit prekreslit(); }
 
 Dokument::Dokument() {
-  _sirka = std::make_unique<QrealVlastnost>("Šírka", 1189);
-  _vyska = std::make_unique<QrealVlastnost>("Výška", 841);
-  connect(_sirka.get(), SIGNAL(hodnotaZmenena(qreal)), this,
-          SLOT(velkostDokumentuZmenena(qreal)));
-  connect(_vyska.get(), SIGNAL(hodnotaZmenena(qreal)), this,
-          SLOT(velkostDokumentuZmenena(qreal)));
+    _sirka = std::make_unique<QrealVlastnost>("Šírka", 1189);
+    _vyska = std::make_unique<QrealVlastnost>("Výška", 841);
+    connect(_sirka.get(), SIGNAL(hodnotaZmenena(qreal)), this,
+            SLOT(velkostDokumentuZmenena(qreal)));
+    connect(_vyska.get(), SIGNAL(hodnotaZmenena(qreal)), this,
+            SLOT(velkostDokumentuZmenena(qreal)));
 
-  auto ciara = std::make_unique<Komponenty::Ciara>();
+    auto ciara = std::make_unique<Komponenty::Ciara>();
 
-  _komponenty.push_back(std::move(ciara));
+    _komponenty.push_back(std::move(ciara));
 }
 
 qreal Dokument::sirka() const { return _sirka->hodnota(); }
@@ -29,72 +29,72 @@ void Dokument::setSirka(qreal sirka) { _sirka->setHodnota(sirka); }
 void Dokument::setVyska(qreal vyska) { _vyska->setHodnota(vyska); }
 
 std::vector<Vlastnost *> Dokument::Vlastnosti() const {
-  return {_sirka.get(), _vyska.get()};
+    return {_sirka.get(), _vyska.get()};
 }
 
 void Dokument::Vykresli(QPainter &painter) {
-  painter.fillRect(0, 0, _sirka->hodnota(), _vyska->hodnota(), Qt::white);
+    painter.fillRect(0, 0, _sirka->hodnota(), _vyska->hodnota(), Qt::white);
 
-  for (auto &k : _komponenty) {
-    k->Vykresli(painter);
-    for (auto &m : k->Manipulatory())
-      m->Vykresli(painter);
-  }
+    for (auto &k : _komponenty) {
+        k->Vykresli(painter);
+        for (auto &m : k->Manipulatory())
+            m->Vykresli(painter);
+    }
 
-  if(_vybranyKomponent)
-  {
-      qreal x1 = _sirka->hodnota();
-      qreal x2 = 0;
-      qreal y1 = _vyska->hodnota();
-      qreal y2 = 0;
-      for(auto&& m: _vybranyKomponent->Manipulatory())
-      {
-          if(auto man = dynamic_cast<Komponenty::Manipulator*>(m.get()))
+    if(_vybranyKomponent)
+    {
+        qreal x1 = _sirka->hodnota();
+        qreal x2 = 0;
+        qreal y1 = _vyska->hodnota();
+        qreal y2 = 0;
+        for(auto&& m: _vybranyKomponent->Manipulatory())
+        {
+            if(auto man = dynamic_cast<Komponenty::Manipulator*>(m.get()))
             {
-              auto b = man->getBod();
-              x1 = qMin(x1, b.x());
-              x2 = qMax(x2, b.x());
-              y1 = qMin(y1, b.y());
-              y2 = qMax(y2, b.y());
+                auto b = man->getBod();
+                x1 = qMin(x1, b.x());
+                x2 = qMax(x2, b.x());
+                y1 = qMin(y1, b.y());
+                y2 = qMax(y2, b.y());
             }
-      }
-      painter.setPen(QPen(QBrush(Qt::red),1));
-      painter.drawRect(QRectF(QPointF(x1,y1) - Komponenty::Manipulator::Polomer(),
-                              QPointF(x2,y2) + Komponenty::Manipulator::Polomer()));
-  }
+        }
+        painter.setPen(QPen(QBrush(Qt::red),1));
+        painter.drawRect(QRectF(QPointF(x1,y1) - Komponenty::Manipulator::Polomer(),
+                                QPointF(x2,y2) + Komponenty::Manipulator::Polomer()));
+    }
 }
 
 void Dokument::VytvorSpojenia(QPointF bod) {
-  std::vector<Komponenty::SpojenieSlot *> sloty;
+    std::vector<Komponenty::SpojenieSlot *> sloty;
 
-  for (auto &&k : _komponenty) {
-    for (auto &&m : k->SpojenieSloty()) {
-		if (m->Obsahuje(bod))
-		{
-			sloty.push_back(dynamic_cast<Komponenty::SpojenieSlot *>(m.get()));
-			break; //maximalne jeden slot z jedneho komponentu
-		}
-    }
-  }
-
-  if (sloty.size() > 1) {
-
-    auto spojenieIterator =
-        std::find_if(_spojenia.begin(), _spojenia.end(),
-                     [bod](auto &&s) { return s->Obsahuje(bod); });
-    std::shared_ptr<Komponenty::Komponent> spojenie;
-	if (spojenieIterator != _spojenia.end())
-		spojenie = *spojenieIterator;
-	else
-	{
-      spojenie = std::make_shared<Komponenty::Spojenie>();
-      _spojenia.push_back(spojenie);
+    for (auto &&k : _komponenty) {
+        for (auto &&m : k->SpojenieSloty()) {
+            if (m->Obsahuje(bod))
+            {
+                sloty.push_back(dynamic_cast<Komponenty::SpojenieSlot *>(m.get()));
+                break; //maximalne jeden slot z jedneho komponentu
+            }
+        }
     }
 
-    for (auto &&slot : sloty) {
-      slot->NastavSpojenie(spojenie.get());
+    if (sloty.size() > 1) {
+
+        auto spojenieIterator =
+                std::find_if(_spojenia.begin(), _spojenia.end(),
+                             [bod](auto &&s) { return s->Obsahuje(bod); });
+        std::shared_ptr<Komponenty::Komponent> spojenie;
+        if (spojenieIterator != _spojenia.end())
+            spojenie = *spojenieIterator;
+        else
+        {
+            spojenie = std::make_shared<Komponenty::Spojenie>();
+            _spojenia.push_back(spojenie);
+        }
+
+        for (auto &&slot : sloty) {
+            slot->NastavSpojenie(spojenie.get());
+        }
     }
-  }
 }
 
 Komponenty::Komponent * Dokumenty::Dokument::Komponent(QPointF bod)
@@ -103,12 +103,12 @@ Komponenty::Komponent * Dokumenty::Dokument::Komponent(QPointF bod)
     if (s != _spojenia.rend())
     {
         _vybranyKomponent = (*s).get();
-		return (*s).get();
+        return (*s).get();
     }
 
     auto&& k = std::find_if(_komponenty.rbegin(), _komponenty.rend(), [bod](auto&& k) {return k->Obsahuje(bod); });
     if (k != _komponenty.rend())
-	{
+    {
         auto&& m = std::find_if((*k)->Manipulatory().rbegin(), (*k)->Manipulatory().rend(), [bod](auto&& m) {return m->Obsahuje(bod); });
         if (m != (*k)->Manipulatory().rend())
         {
@@ -120,7 +120,7 @@ Komponenty::Komponent * Dokumenty::Dokument::Komponent(QPointF bod)
             _vybranyKomponent = (*k).get();
             return (*k).get();
         }
-	}
+    }
 
     _vybranyKomponent = nullptr;
     return nullptr;
@@ -140,10 +140,15 @@ Komponenty::Komponent *Dokument::vybranyKomponent() const
 void Dokument::VycistiSpojenia()
 {
     std::remove_if(_spojenia.begin(), _spojenia.end(),
-                   [](auto& s) {
+                   [this](auto& s) {
         if(auto spojenie = dynamic_cast<Komponenty::Spojenie*>(s.get()))
-            return spojenie->JePrazdne();
-        else
+        {
+            if(spojenie->JePrazdne()) {
+                if(_vybranyKomponent == spojenie)
+                    _vybranyKomponent = nullptr;
+                return true;
+            }
             return false;
+        }
     });
 }
